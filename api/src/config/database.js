b4dfba4e114a33055e51029e34a461063c2ee91e
rzlk11 <rizalharyo14@gmail.com>
2025-06-07 20:52:28 +0700

    tes db

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -2,13 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || "bizly_db",
-  process.env.DB_USER || "postgres",
-  process.env.DB_PASSWORD || "sekandri",
+const sequelize = new Sequelize(process.env.DB_URI, 
   {
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 5432,
     dialect: "postgres",
   }
 );
