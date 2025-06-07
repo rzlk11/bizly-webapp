c98dfea5d133ff202ac916742fd7224fa123554d
rzlk11 <rizalharyo14@gmail.com>
2025-06-07 23:53:24 +0700

    change port

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ import mlRoute from "./src/routes/mlRoute.js";
 import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.json());
